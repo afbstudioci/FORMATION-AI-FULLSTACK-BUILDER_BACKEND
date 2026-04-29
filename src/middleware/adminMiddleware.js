d7d61b1206dfd2307785a6ebabcf76e120436b33
baticlean <baticlean225@gmail.com>
2026-04-29 03:24:11 +0000

    correction

--- src/middleware/adminMiddleware.js
+++ src/middleware/adminMiddleware.js
@@ -1,4 +1,9 @@
 const verifyAdminPassword = (req, res, next) => {
+  // Bypass automatique pour l'Admin Principal (Master Root)
+  if (req.user && req.user._id === "000000000000000000000000") {
+    return next();
+  }
+
   const adminPassword = req.headers['x-admin-password'];
 
   if (!adminPassword || adminPassword !== process.env.PASSWORD_ADMIN) {
