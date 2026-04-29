aeb6f13dca1e6654de60def6089b022e3852e312
baticlean <baticlean225@gmail.com>
2026-04-29 22:48:56 +0000

    correction

--- src/models/User.js
+++ src/models/User.js
@@ -15,14 +15,14 @@ const userSchema = mongoose.Schema({
 });
 
 // Hashage du mot de passe avant sauvegarde
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Methode pour comparer les mots de passe
-userSchema.methods.matchPassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
